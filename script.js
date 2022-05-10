c953d4a852e127cb323d45adea7e4326b9f35de5
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-05-10 14:49:28 +0500

    play pause javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+const playPauseBtn = document.querySelector(".play-pause-btn");
+const video = document.querySelector("video");
+const videoContainer = document.querySelector(".video-container");
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key.toLowerCase()) {
+    case " ":
+    case "k":
+      togglePlay();
+      break;
+  }
+});
+
+// Play / Pause
+playPauseBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+
+video.addEventListener("play", () => {
+  videoContainer.classList.remove("paused");
+});
+
+video.addEventListener("pause", () => {
+  videoContainer.classList.add("paused");
+});

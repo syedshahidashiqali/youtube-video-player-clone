84932bded6cc4280c0e76812727e2314fe15b4c6
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-05-14 19:16:24 +0500

    fullscreen, theater, miniplayer javascript

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const playPauseBtn = document.querySelector(".play-pause-btn");
+const miniPlayerBtn = document.querySelector(".mini-player-btn");
+const theaterBtn = document.querySelector(".theater-btn");
+const fullScreenBtn = document.querySelector(".full-screen-btn");
 const video = document.querySelector("video");
 const videoContainer = document.querySelector(".video-container");
 
@@ -9,6 +12,43 @@ document.addEventListener("keydown", (e) => {
       togglePlay();
       break;
   }
+});
+
+// View Modes
+theaterBtn.addEventListener("click", toggleTheaterMode);
+fullScreenBtn.addEventListener("click", toggleFullScreenMode);
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode);
+
+function toggleTheaterMode() {
+  videoContainer.classList.toggle("theater");
+}
+
+function toggleFullScreenMode() {
+  if (document.fullscreenElement == null) {
+    videoContainer.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+function toggleMiniPlayerMode() {
+  if (videoContainer.classList.contains("mini-player")) {
+    document.exitPictureInPicture();
+  } else {
+    video.requestPictureInPicture();
+  }
+}
+
+document.addEventListener("fullscreenchange", () => {
+  videoContainer.classList.toggle("full-screen", document.fullscreenElement);
+});
+
+video.addEventListener("enterpictureinpicture", () => {
+  videoContainer.classList.add("mini-player");
+});
+
+video.addEventListenere("leavepictureinpicture", () => {
+  videoContainer.classList.remove("mini-player");
 });
 
 // Play / Pause
